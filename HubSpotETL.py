cb4950a454a0acda6e9aa99ff437b4f6223c5ba9
ndangelo1-kfcu <131177463+ndangelo1-kfcu@users.noreply.github.com>
2025-06-20 13:44:08 -0500

    update to get all files in extract folder

--- HubSpotETL.py
+++ HubSpotETL.py
@@ -104,12 +104,15 @@ def copy_files_to_network(files, PROCESSDATE, source_folder, network_folder):
         sys.exit(1)
         raise
 
-    # Copy all all other EXTRACT.* files:
-    # Find all files that match EXTRACT.*
-    pattern = os.path.join(os.path.join(source_folder, PROCESSDATE), "EXTRACT.*")
-    extract_files = glob.glob(pattern)
+    # Copy all files in the source folder for the given PROCESSDATE
+    source_folder_path = os.path.join(source_folder, PROCESSDATE)
+    all_files = [
+        os.path.join(source_folder_path, f)
+        for f in os.listdir(source_folder_path)
+        if os.path.isfile(os.path.join(source_folder_path, f))
+    ]
     try:
-        for src_path in extract_files:
+        for src_path in all_files:
             filename = os.path.basename(src_path)
             if filename == "EXTRACT.CARD":
                 continue  # Skip EXTRACT.CARD
@@ -135,6 +138,13 @@ def mask_column(series):
             if pd.notnull(x) and len(str(x)) > 4
             else x
         )
+    )
+
+
+def slice_last4_column(series):
+    # Return only the last 4 characters of each value
+    return series.apply(
+        lambda x: str(x)[-4:] if pd.notnull(x) and len(str(x)) > 4 else x
     )
 
 
@@ -168,6 +178,16 @@ def process_extract_card(PROCESSDATE):
     df = pd.read_csv(dest_path, delimiter="~", header=None, dtype=str)
     # Mask the 3rd column (index 2)
     df[2] = mask_column(df[2])
+
+    # # Add two null columns to the end of df
+    # df[len(df.columns)] = None
+    # df[len(df.columns)] = None
+
+    # Print and log the number of columns
+    num_columns = df.shape[1]
+    print(f"Number of columns in DataFrame: {num_columns}")
+    logger.info(f"Number of columns in DataFrame: {num_columns}")
+
     print("Processed EXTRACT.CARD file and masked the 3rd column.")
     logger.info("Processed EXTRACT.CARD file and masked the 3rd column.")
     return df
@@ -188,7 +208,7 @@ def zip_network_folder(network_folder, PROCESSDATE):
 
 def save_masked_card_extract(df, staging_folder):
     # Save the processed DataFrame to CSV in the same format as the original
-    csv_file_path = os.path.join(staging_folder, f"EXTRACT.CARD_masked.txt")
+    csv_file_path = os.path.join(staging_folder, f"EXTRACT.CARD")
     df.to_csv(
         csv_file_path,
         sep="~",
@@ -285,7 +305,7 @@ def main(start_time):
         save_masked_card_extract(df, STAGING_FOLDER)
 
         # Add the CSV file path to the list of files to copy
-        files.append((STAGING_FOLDER, f"EXTRACT.CARD_masked.txt"))
+        files.append((STAGING_FOLDER, f"EXTRACT.CARD"))
 
         # Copy files to the network folder
         print("Copying files to network folder...")
